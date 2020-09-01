d73e0369b4c7f4cebe2298e937da05d1c1630b48
Manyama <jaron.manyama@sap.com>
2020-09-01 08:23:49 -0700

    test commit

--- blog_app.js
+++ blog_app.js
@@ -19,9 +19,9 @@ useUnifiedTopology: true
   //mongoose.connect('mongodb://localhost:27017/restful_blog_app', {
   //useNewUrlParser: true,
  // useUnifiedTopology: true
-})
-.then(() => console.log('Connected to DB!'))
-.catch(error => console.log(error.message));
+//})
+//.then(() => console.log('Connected to DB!'))
+//.catch(error => console.log(error.message));
 
 //INITIATE BODY PARSER FOR GATHERING FORM DATA
 app.use(bodyParser.urlencoded({extended: true}));
